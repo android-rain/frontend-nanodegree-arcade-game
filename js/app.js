5787c18c3cf93c1d3266dd47d7c087b3173d6ef8
android-rain <yy924224@gmail.com>
2017-05-21 11:57:15 +0800

    first step: add a moving bug

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    var x = -100;
+    var y = 0;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +15,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < 505){
+        this.x += dt*100;
+    }
+    else {
+        this.x = -100;
+    }
+    this.y = 60;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +32,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    var x =
+    this.sprite = "images/char-boy.png";
+};
 
+Player.prototype.update = function(dt) {
 
+};
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function() {
+
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+var numOfEnemies = 1;
+for(var i = 0; i < numOfEnemies; i++){
+  allEnemies.push(new Enemy());
+}
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
